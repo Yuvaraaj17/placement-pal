7f3da7a7ced2a46aba7999636e9bee518dff696e
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2025-12-29 10:18:30 +0530

    Added Sidebar to page Layout

---
+++ app/layouts/page.vue
@@ -0,0 +1,64 @@
+<script setup>
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from '@/components/ui/sidebar'
+import { Calendar, FlaskConical, House, LayoutDashboard } from 'lucide-vue-next'
+
+const items = [
+  {
+    title: 'Home',
+    url: '/home',
+    icon: House,
+  },
+  {
+    title: 'Dashboard',
+    url: '/dashboard',
+    icon: LayoutDashboard,
+  },
+  {
+    title: 'Test',
+    url: '/test',
+    icon: FlaskConical,
+  },
+  {
+    title: 'Drives',
+    url: '/drive',
+    icon: Calendar,
+  },
+]
+</script>
+
+<template>
+  <SidebarProvider>
+    <Sidebar variant="sidebar">
+      <SidebarHeader>
+        <span class="rounded-md bg-slate-50 p-3 shadow">Welcome Yuvaraaj</span>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton as-child>
+                  <a :href="item.url">
+                    <component :is="item.icon" />
+                    <span>{{ item.title }}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+    <slot />
+  </SidebarProvider>
+</template>
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,5 +1,11 @@
+<script>
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
 <template>
-  <p class="text-muted-foreground text-sm italic">
+  <p class="text-muted-foreground w-full text-sm italic">
     Welcome to Placement Pal! Your journey to acing interviews starts here.
   </p>
 </template>
